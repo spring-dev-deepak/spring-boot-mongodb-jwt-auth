d77054448dfa4d1a88e82553383cae0db8f4352a
spring-dev-deepak <65223428+spring-dev-deepak@users.noreply.github.com>
2020-09-05 09:46:24 +0530

    Initial commit.

---
+++ src/main/java/com/spring/deepak/jwtsecuritymongodb/JwtSecurityMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.spring.deepak.jwtsecuritymongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/deepak/jwtsecuritymongodb/JwtSecurityMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.deepak.jwtsecuritymongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
